cf6ad411fce70576ed9a84656a67b6c916d2c7ef
clark <cxt474@student.bham.ac.uk>
2024-11-25 13:25:56 +0000

    Removed unncessary code encryption.c

--- encryption.c
+++ encryption.c
@@ -4,11 +4,6 @@
 #include <openssl/err.h>
 #include <openssl/rand.h>
 
-int keyGen(unsigned char salt[], char* out);
-int keyGenNoSalt(char* outKey, char* outSalt);
-int encrypt(char* fileName);
-int decrypt(char* fileName);
-
 // Converts a string of bytes into hex (Used for debugging primarily)
 void to_hex_string(const unsigned char *data, size_t len, char *out) {
     for (size_t i = 0; i < len; i++) {
@@ -86,14 +81,8 @@ int encrypt(char* fileName){
   }
   encBuf[fileSize] = '\0';
 
-  // Used for Debugging
-  //printf("Key used: %s \n", key);
-  //printf("Salt used: %s \n", salt);
-  //printf("Output: %s \n", encBuf);
-
-  // Creates a final buffer with the encrypted message and the salt used.
+  // Creates a final buffer with the encrypted message and the salt used. The first 16 bytes will be the salt
   char *outputBuf = malloc((fileSize + 16) * sizeof(char));
-  
   memcpy(outputBuf, salt, 16);
   memcpy(outputBuf + 16, encBuf, fileSize);
   outputBuf[fileSize + 16] = '\0';
@@ -102,10 +91,10 @@ int encrypt(char* fileName){
   fprintf(file, "%s", outputBuf);
   fclose(file);
 
-
   free(fileBuf);
   free(encBuf);
   free(outputBuf);
+
   printf("%s Successfully encrypted\n", fileName);
 
   FILE *logFile = fopen("log", "a"); 
@@ -117,30 +106,33 @@ int encrypt(char* fileName){
 
 // XOR decryption 
 int decrypt(char* fileName){
+
   FILE *file = fopen(fileName, "rb");
   fseek(file, 0, SEEK_END);
   size_t fileSize = ftell(file);
   rewind(file);
 
+  // If the file is less than 16 bytes there is no salt at the start as this creates a minimum of 16 bytes. It must not be encrypted
   if(fileSize < 16){
     printf("File cannot be decrypted\n");
     return 1;
   }
 
+  // The first 16 bytes are read as the salt and a key is generated from this
   char salt[16];
-  char salt_hex[33];
-  fread(salt, 1, 16, file);
-
   char key[16];
+  fread(salt, 1, 16, file);
   keyGen(salt, key);
 
   char *fileBuf = malloc((fileSize - 16) * sizeof(char));
   char *outBuf = malloc((fileSize - 16) * sizeof(char));
 
+  // The remaining bytes of the file are read to a buffer
   fread(fileBuf, 1, fileSize - 16, file);
   fclose(file);
   fileBuf[fileSize -16] = '\0';
 
+  // Iterates through the file and performs an XOR decryption. 
   for (int i = 0; i < fileSize - 16; i++){
     outBuf[i] = fileBuf[i] ^ key[i % 16];
   }
